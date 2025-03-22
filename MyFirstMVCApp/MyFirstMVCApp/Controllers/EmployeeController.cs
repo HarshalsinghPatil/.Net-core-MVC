5aa14047a48650b4a20ece5edf3b8d5b46007069
HarshalsinghPL <Harshalsinghrajput@gmail.com>
2025-03-23 01:00:32 +0530

    Ways of passing data from controller to view (Viewbag, Viewdata, Tempdata)

--- MyFirstMVCApp/MyFirstMVCApp/Controllers/EmployeeController.cs
+++ MyFirstMVCApp/MyFirstMVCApp/Controllers/EmployeeController.cs
@@ -2,18 +2,45 @@
 
 namespace MyFirstMVCApp.Controllers
 {
-    [Route("staff")]//Routing for controller
+    [Route("staff")]
     public class EmployeeController : Controller
     {
-        [Route("list")]// staff/list
-        public IActionResult List()
+
+        // We can pass data from a controller to a view using:
+        //1 ViewBag : ViewBag dynamically stores data that can be used in a View.
+        //2 ViewData :ViewData works similarly to ViewBag, but it uses a dictionary.
+        //3 TempData : is used for passing data between different requests (useful for redirects).
+
+        //1. ViewBag
+        [Route("index")]
+        public IActionResult Index()
         {
             ViewBag.Message = "Employee List Page";
             return View();
         }
 
-        [Route("create")]//staff/create
-        [HttpPost]//Post method
+
+        //2. ViewData
+
+        [Route("list")]
+        public IActionResult List()
+        {
+            ViewData["Message"] = "Welcome to the Employee List Page!";
+            return View();
+        }
+
+        //3. TempData
+        [HttpGet]
+        [Route("save")]
+        public IActionResult Save()
+        {
+            TempData["SuccessMessage"] = "Employee record saved successfully!";
+            return RedirectToAction("List");
+        }
+
+
+        [Route("create")]
+        [HttpPost]
         public IActionResult Create(string name, string department)
         {
             return Content($"Employee Created: {name}, Department: {department}");
