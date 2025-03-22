f5129d63edaf6bb66971a9d207a9930ff81fecfb
HarshalsinghPL <Harshalsinghrajput@gmail.com>
2025-03-23 00:18:06 +0530

    Pass data through viewbag

---
+++ MyFirstMVCApp/MyFirstMVCApp/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstMVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        //Index() is an Action Method that will return a View.
+        public IActionResult Index()
+        {
+            // Passing data through viewbag
+            ViewBag.Message = "Hello from EmployeeController!";
+            return View();
+            //tells the framework to look for a View(in folder Views/Employee/Index) with the same name (Index.cshtml) .
+        }
+
+
+
+
+    }
+}
